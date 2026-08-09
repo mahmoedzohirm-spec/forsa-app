dc01a697b7d5f492096b6e48a93bc4e8f2f16215
Mahmoud Zuhair Mahmoud Abu Hilal <mahmoedzohirm@gmail.com>
2026-08-09 17:37:37 +0200

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/admin/', '/auth/'],
     },
-    sitemap: 'https://forsa-app-lovat.vercel.app/sitemap.xml',
+    sitemap: 'https://forsa-app-ten.vercel.app/sitemap.xml',
   };
 }
