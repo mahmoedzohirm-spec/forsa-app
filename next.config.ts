11c7967ccc3108f8598a8cc017a9f20cedc77048
Mahmoud Zuhair Mahmoud Abu Hilal <mahmoedzohirm@gmail.com>
2026-08-13 13:37:14 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type { NextConfig } from "next";
 import withPWA from "next-pwa";
 import createNextIntlPlugin from 'next-intl/plugin';
@@ -10,23 +11,8 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '10mb',
     },
   },
-  // ✅ إضافة رؤوس CORS لجميع APIs
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: 'https://forsa-app-ten.vercel.app' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-        ],
-      },
-    ];
-  },
 };
 
-// تطبيق الـ PWA أولاً ثم الـ i18n
 export default withNextIntl(withPWA({
   dest: "public",
   register: true,
